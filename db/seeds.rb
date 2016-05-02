f169be8c392484946e173d92f0dcea67d2715426
Brent Martin <bmart35@gmail.com>
2016-05-01 22:37:34 -0500

    Increased the ammount of users in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-5.times do |n|
+10.times do |n|
   User.create!(username: Faker::Internet.user_name, password: "password")
 end
 

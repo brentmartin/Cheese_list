eb8ff43fc7b92785174c47a7fcdc129d413b0bc4
Brent Martin <bmart35@gmail.com>
2016-08-03 17:45:58 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,47 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do |n|
+@cheese = [
+  "Gouda",
+  "Brie",
+  "Cheddar",
+  "Goat Cheese",
+  "Feta",
+  "Mozzarella",
+  "Cotija",
+  "Swiss",
+  "Monterey Jack",
+  "Parmigiano-Reggiano",
+  "Provolone",
+  "Gorgonzola",
+  "Havarti",
+  "Ricotta",
+  "Colby-Jack",
+  "Emmentaler",
+  "Camembert",
+  "Manchego",
+  "Pecorino Romano",
+  "Asiago",
+  "Gruyere",
+  "Smoked Gouda",
+  "Limburger",
+  "Munster",
+  "Neufchatel",
+  "Queso Blanco",
+  "Beuafort D'ete",
+  "Gorau Glas",
+  "Rogue River Blue",
+  "Winnimere",
+  "Cacio Bufala",
+  "Jersey Blue",
+  "Old Ford",
+  "Caciocavallo Podolico"
+]
+
+10.times do
   User.create!(username: Faker::Internet.user_name, password: "password")
 end
 
-
-
-
-Cheese.create!(name: "Gouda")
-Cheese.create!(name: "Brie")
-Cheese.create!(name: "Cheddar")
-Cheese.create!(name: "Goat Cheese")
-Cheese.create!(name: "Feta")
-Cheese.create!(name: "Mozzarella")
-Cheese.create!(name: "Cotija")
-Cheese.create!(name: "Swiss")
-Cheese.create!(name: "Monterey Jack")
-Cheese.create!(name: "Parmigiano-Reggiano")
-Cheese.create!(name: "Provolone")
-Cheese.create!(name: "Gorgonzola")
-Cheese.create!(name: "Havarti")
-Cheese.create!(name: "Ricotta")
-Cheese.create!(name: "Colby-Jack")
-Cheese.create!(name: "Emmentaler")
-Cheese.create!(name: "Camembert")
-Cheese.create!(name: "Manchego")
-Cheese.create!(name: "Pecorino Romano")
-Cheese.create!(name: "Asiago")
-Cheese.create!(name: "Gruyere")
-Cheese.create!(name: "Smoked Gouda")
-Cheese.create!(name: "Limburger")
-Cheese.create!(name: "Munster")
-Cheese.create!(name: "Neufchatel")
-Cheese.create!(name: "Queso Blanco")
-Cheese.create!(name: "Beuafort D'ete")
-Cheese.create!(name: "Gorau Glas")
-Cheese.create!(name: "Rogue River Blue")
-Cheese.create!(name: "Winnimere")
-Cheese.create!(name: "Cacio Bufala")
-Cheese.create!(name: "Jersey Blue")
-Cheese.create!(name: "Old Ford")
-Cheese.create!(name: "Caciocavallo Podolico")
+@cheese.map do |c|
+  c = Cheese.create(name: c)
+end
